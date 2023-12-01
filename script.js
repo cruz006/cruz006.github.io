a2f5326f8a36a22f17a5a9d237ff1ee78601af5f
cruz006 <vans5364@gmail.com>
2023-12-01 12:15:05 -0800

    use title code from https://github.com/cruz006/htpamesa

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function() {
+    var message = "come back .(._. ).";
+    var original;
+  
+    $(window).focus(function() {
+      if (original) {
+        document.title = original;
+      }
+    }).blur(function() {
+      var title = $('title').text();
+      if (title != message) {
+        original = title;
+      }
+      document.title = message;
+    });
+  });
+  
